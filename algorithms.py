ee8f0d4fb4dfe752bc96409e4e298d18b42bd2be
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-04-13 12:44:21 +0200

    set upt important files for the script

---
+++ algorithms.py
@@ -0,0 +1,6 @@
+import networkx as nx  #for optimal modularity algorithms (greedy, louvain)
+import networkx.algorithms.community as nx_comm
+import igraph as ig    #other algorithms for community detection (edge_betweeness)
+
+import pandas as pd
+from matplotlib import pyplot as plt
