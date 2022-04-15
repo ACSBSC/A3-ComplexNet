e3c912066e05e98473070470719c4dc043f7d22e
rubi1100 <65720112+rubi1100@users.noreply.github.com>
2022-04-15 12:00:52 +0200

    Main automatizado y listo para el compare.

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-import networkx as nx  #for optimal modularity algorithms (greedy, louvain)
-import networkx.algorithms.community as nx_comm
-import igraph as ig    #other algorithms for community detection (edge_betweeness)
+import os
+import glob
 import sklearn as sk
 import pandas as pd
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+
+import networkx as nx  # for optimal modularity algorithms (greedy, louvain)
+import networkx.algorithms.community as nx_comm
+import igraph as ig    # other algorithms for community detection (edge_betweeness)
 
 import algorithms as al
 
-network_airport = './A3-networks/real/cat_cortex_sim.net'
-graph = nx.read_pajek(network_airport)
-graph = nx.Graph(graph)
 
-#al.louvain(graph)
-#al.greedy(graph)
-al.walktrap(graph)
+if __name__ == '__main__':
+    folders = ["./A3-networks/model/", "./A3-networks/real/", "./A3-networks/toy/"]
+
+    for folder in folders:
+        net_files = glob.glob(os.path.join(folder, "*.net"))
+        for net_file in net_files:
+            net_file_sep = net_file.split("\\")
+            net_file_sep = net_file_sep[1].split(".net")
+
+            if net_file_sep[0] != "rb125":
+                clu_file = folder + net_file_sep[0] + ".clu"
+            else:
+                clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]
+
+            graph = nx.read_pajek(net_file)
+            graph = nx.Graph(graph)
+
+            # al.louvain(graph)
+            # al.greedy(graph)
+            al.walktrap(graph)
+
+    """
+    network_airport = './A3-networks/real/cat_cortex_sim.net'
+    graph = nx.read_pajek(network_airport)
+    graph = nx.Graph(graph)
+    
+    # al.louvain(graph)
+    # al.greedy(graph)
+    al.walktrap(graph)
+    """
