709a55bce120a3a6c834be8360a89e248564c4f5
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-04-16 11:55:11 +0200

    get all existing clu files within the folders

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import os
 import glob
-import sklearn as sk
-import pandas as pd
-from matplotlib import pyplot as plt
 
 import networkx as nx  # for optimal modularity algorithms (greedy, louvain)
-import networkx.algorithms.community as nx_comm
-import igraph as ig    # other algorithms for community detection (edge_betweeness)
+
 
 import algorithms as al
 
 
 if __name__ == '__main__':
     folders = ["./A3-networks/model/", "./A3-networks/real/", "./A3-networks/toy/"]
+    
+    community_list_louvain=[]
+    community_list_greedy=[]
+    community_list_walktrap=[]
 
     for folder in folders:
         net_files = glob.glob(os.path.join(folder, "*.net"))
-        clu_files = glob.glob(os.path.join(folder, "*.clu"))
+        #clu_files = glob.glob(os.path.join(folder, "*.clu"))
         for net_file in net_files:
             net_file_sep = net_file.split("\\")
             net_file_sep = net_file_sep[1].split(".net")
 
-            if net_file_sep[0] == "rb125":
+            '''if net_file_sep[0] == "rb125":
                 #clu_file = folder + net_file_sep[0] + ".clu"
                 clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]
             elif net_file_sep[0] == "dolphins":
@@ -32,17 +32,24 @@ if __name__ == '__main__':
                 clu_file = folder + net_file_sep[0] + "-real.clu"
             else:
                 clu_file = folder + net_file_sep[0] + ".clu"
-                #clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]
+                #clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]'''
 
             graph = nx.read_pajek(net_file)
             graph = nx.Graph(graph)
-
-            #print(net_file_sep[0])
             
             community_list_louvain=al.louvain(graph, net_file_sep[0])
-            #community_list_greedy=al.greedy(graph, net_file_sep[0])
-            #community_list_walktrap=al.walktrap(graph, net_file_sep[0])
+            community_list_greedy=al.greedy(graph, net_file_sep[0])
+            community_list_walktrap=al.walktrap(graph, net_file_sep[0])
+            
+        for folder in folders:
+            clu_files = glob.glob(os.path.join(folder, "*.clu"))
+            for clu_file in clu_files:
+                clu_file_sep = clu_file.split("\\")
+                clu_file_sep = clu_file_sep[1].split(".clu")
         #print(clu_files)
+    
+    
+    
     
     '''network_airport = './A3-networks/real/airports_UW.net'
     graph = nx.read_pajek(network_airport)
