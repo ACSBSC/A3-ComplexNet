36f285b218d5036475175cbfc6cd40b44cf8f1f5
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-04-16 22:41:52 +0200

    tryout plots

--- main.py
+++ main.py
@@ -17,27 +17,25 @@ if __name__ == '__main__':
             net_file_sep = net_file.split("\\")
             net_file_sep = net_file_sep[1].split(".net")
 
-            '''if net_file_sep[0] == "rb125":
-                #clu_file = folder + net_file_sep[0] + ".clu"
-                clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]
-            elif net_file_sep[0] == "dolphins":
-                clu_file = folder + net_file_sep[0] + "-real.clu"
-            elif net_file_sep[0] == "football":
-                clu_file = folder + net_file_sep[0] + "-conferences.clu"
-            elif net_file_sep[0] == "zachary_unwh":
-                clu_file = folder + net_file_sep[0] + "-real.clu"
-            else:
-                clu_file = folder + net_file_sep[0] + ".clu"
-                #clu_file = [folder + "rb125-1.clu", folder + "rb125-2.clu", folder + "rb125-3.clu"]'''
-
             graph = nx.read_pajek(net_file)
             graph = nx.Graph(graph)
             
-            al.louvain(graph, net_file_sep[0])
+            '''al.louvain(graph, net_file_sep[0])
             al.greedy(graph, net_file_sep[0])
-            al.walktrap(graph, net_file_sep[0])
-                   
-    c.compare_partitions()   
+            al.walktrap(graph, net_file_sep[0])'''
+    
+    network_airport = './A3-networks/real/airports_UW.net'
+    graph = nx.read_pajek(network_airport)
+    graph = nx.Graph(graph)
+    al.louvain(graph, "airports_UW") 
+    al.greedy(graph, "airports_UW")
+    al.walktrap(graph, "airports_UW")            
+    #c.compare_partitions()   
+    
+    
+    
+    
+   
     
     
     
--- ploting.py
+++ ploting.py
@@ -31,4 +31,36 @@ def plot(cluster_nodes, G, path, algorithm):
 
         plt.savefig("./plots/"+path+'/'+algorithm+'_plot.png')
         plt.show()
+
+
+#El siguiente código es una copia del anterior pero para hacer pruebas    
+'''def plot(cluster_nodes, G, path, algorithm):  
     
+    print("Plottin Network "+path+" with algorithm "+algorithm)
+    G = nx.Graph(G)
+    
+    colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
+    colors = list(colors)
+    
+    #calculate position for each node
+    # pos is needed because we are going to draw a few nodes at a time,
+    # pos fixes their positions.
+    
+    if path != 'airports_UW':
+        pos = nx.spring_layout(G)                   #Esta es a línea que trae problemas
+    else:
+        pos = nx.planar_layout(G) 
+    print(type(pos))
+    # Notice that the pos dict is passed to each call to draw below
+
+    # Draw the graph, but don't color the nodes
+    plt.figure(3,figsize=(12,10)) 
+    nx.draw(G, pos, edge_color='k',  #with_labels=True,
+            font_weight='light', node_size= 280, width= 0.9)
+    
+    for i in range(len(cluster_nodes)):
+        nx.draw_networkx_nodes(G, pos, nodelist=cluster_nodes[i], node_color=colors[i*5])
+
+
+    plt.savefig("./plots/"+path+'/'+algorithm+'_plot.png')
+    plt.show()'''
